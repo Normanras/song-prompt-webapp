e1c61bec3d2c77fb1a3a246645c41c35accd9f10
Norm Rasmussen <norman.rasmussen@me.com>
2023-12-04 13:42:06 -0500

    Got the database setup and am able to pull a random word from the query. I can read the themes table and have the async functions setup, but not yet working as expected. There are a few test scripts in there that need to be cleaned up before pushing to main.

--- gpt4all_tst.py
+++ gpt4all_tst.py
@@ -2,17 +2,38 @@ from gpt4all import GPT4All
 import requests
 from pathlib import Path
 
-MODEL = GPT4All(
-    model_name="gpt4all-falcon-q4_0.gguf",
-#    model_path=(Path.home() / ".cache" / "gpt4all"),
-# /root/.pyenv/versions/gpt-song-prompt/lib/python3.10/site-packages/gpt4all
-    allow_download=True,
-)
-WORD_PROMPT = str(requests.get("https://random-word-api.herokuapp.com/word").text)[2:-2]
-SYSTEM_TEMPLATE = 'A single sentence based on a word.'
-PROMPT_TEMPLATE = '### Instruction: {0} \n### Response: '
-with MODEL.chat_session(SYSTEM_TEMPLATE, PROMPT_TEMPLATE):
-    response = MODEL.generate(f"A single sentence about {WORD_PROMPT}.", temp=0.7)
-    print(WORD_PROMPT)
-    resp = str(response.splitlines()[0])
-    print(resp)
+
+def prompt():
+    MODEL = GPT4All(
+        model_name="gpt4all-falcon-q4_0.gguf",
+        #    model_path=(Path.home() / ".cache" / "gpt4all"),
+        # /root/.pyenv/versions/gpt-song-prompt/lib/python3.10/site-packages/gpt4all
+        allow_download=False,
+    )
+    WORD_PROMPT = str(requests.get("https://random-word-api.herokuapp.com/word").text)[
+        2:-2
+    ]
+    SYSTEM_TEMPLATE = "A creative response about a word."
+    PROMPT_TEMPLATE = "### Instruction: {0} \n### Response: "
+    response = MODEL.generate(
+        f"Tell me something interesting about {WORD_PROMPT}.",
+        temp=0.7,
+        callback=stop_on_token_callback,
+    )
+    print(response)
+
+    # with MODEL.chat_session(SYSTEM_TEMPLATE, PROMPT_TEMPLATE):
+    #     response = MODEL.generate(f"A single sentence about {WORD_PROMPT}.", temp=0.7)
+    #     print(WORD_PROMPT)
+    #     resp = str(response.splitlines()[0])
+    #     print(resp)
+
+
+def stop_on_token_callback(token_id, token_string):
+    if "." in token_string:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    prompt()
